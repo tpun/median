b9773d7b0a20504ccacc5808733ed24c541125c9
Thomas Pun <thomas@nowbox.com>
2012-08-07 18:50:19 -0700

    Median#add

--- median.rb
+++ median.rb
@@ -4,6 +4,18 @@ class Median
   end
 
   def add n
+    @sorted << n
+    @sorted.sort!
+
+    if @sorted.length.odd?
+      return @sorted[@sorted.length/2].to_s
+    else
+      middle2 = @sorted.length / 2
+      middle1 = middle2 - 1
+      median = (@sorted[middle1] + @sorted[middle2])/2.0
+      median = median.to_i if median.to_i==median
+      return median.to_s
+    end
   end
 
   def remove n
--- median_spec.rb
+++ median_spec.rb
@@ -3,7 +3,36 @@ describe 'Median' do
   subject { Median.new }
 
   describe '#add' do
-    it 'adds input and output median'
+    context 'when the list is odd number of numbers' do
+      it 'adds input and outputs the middle number' do
+        subject.add 10
+        subject.add 20
+
+        median = subject.add 30
+
+        median.should == "20"
+      end
+    end
+
+    context 'when the list is even number of numbers' do
+      it 'adds input and outputs the median and drops the decimal point' do
+        subject.add 10
+        subject.add 20
+        subject.add 30
+
+        median = subject.add 40
+        median.should == "25"
+      end
+
+      it 'adds input and outputs the average of the middle 2 numbers with decimal points' do
+        subject.add 10
+        subject.add 20
+        subject.add 30
+
+        median = subject.add 21
+        median.should == "20.5"
+      end
+    end
   end
 
   describe '#remove' do
